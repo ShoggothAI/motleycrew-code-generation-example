120dcc68627c95a80fda664fef53926571993df6
User <serfintw@yadnex.ru>
2024-06-05 15:56:50 +0300

    add math functions

---
+++ math_functions.py
@@ -0,0 +1,17 @@
+from typing import Union
+
+
+def plus(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    return a + b
+
+
+def minus(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    return a - b
+
+
+def mul(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    return a * b
+
+
+def div(a: Union[int, float], b: Union[int, float]) -> Union[int, float]:
+    return a / b
